c72e1978cfc05e4c4bf8242bb36338f82577c28d
Maxym <maxymbachynskyi@gmail.com>
2018-12-17 20:58:01 +0200

    Fix time

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const bot = new TeleBot({
 });
 
 const DAY = 24 * 60 * 60 * 1000;
-const UKRAIN_GMT = 2;
+const UKRAIN_GMT = 2 * 60 * 60 * 1000;
 
 const getCounters = () => {
 	const electionDay = new Date('2019/03/31 00:00:00');
 	const now = new Date();
-	const diff = electionDay - now + (now.getTimezoneOffset() * 60 * 1000);
+	const diff = electionDay - now + (now.getTimezoneOffset() * 60 * 1000) - UKRAIN_GMT;
 	const restDate = new Date(diff % DAY);
 	const days = Math.round(diff / DAY);
-	const hours = restDate.getHours() - UKRAIN_GMT;
+	const hours = restDate.getHours();
 	const minutes = restDate.getMinutes() + 1;
 
 	return [days, hours, minutes];
